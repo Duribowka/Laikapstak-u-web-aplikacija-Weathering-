946acded3c75890df347a1d5250214e22e60ab13
Duribowka <s.boronin08@gmail.com>
2026-08-15 21:55:38 +0300

    v3.4.4.2
    Testing done using ZAP. Fixed app's vulnerabilities

--- 02_source_code/backend/server.py
+++ 02_source_code/backend/server.py
@@ -2,4 +2,23 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(
+    app,
+    origins=["http://localhost:8080"]
+)
+
+@app.after_request
+def add_security_headers(response):
+
+    response.headers["Content-Security-Policy"] = (
+        "default-src 'none'; "
+        "frame-ancestors 'none';"
+    )
+
+    response.headers["X-Content-Type-Options"] = "nosniff"
+
+    response.headers["X-Frame-Options"] = "DENY"
+
+    response.headers.pop("Server", None)
+
+    return response
